02020ce64ac0cd6c7eca8114bfb6882304e6a88a
yanyufan <yan03510592@163.com>
2017-11-22 12:11:07 +0800

    login_layout

--- app/src/main/java/com/example/heixingxing/dormitory_select/MainActivity.java
+++ app/src/main/java/com/example/heixingxing/dormitory_select/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.heixingxing.dormitory_select;
 
+import android.app.Activity;
+import android.view.View;
+
 /**
  * Created by heixingxing on 2017/10/26.
  */
 
-public class MainActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View v){
+        if(v.getId()==R.id.submit){
+
+        }
+    }
 }
